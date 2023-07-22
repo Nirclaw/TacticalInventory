3cc3c6a6a2a3bf4cf025685a7c99af93bfa945fb
Nirclaw <caicedonicolas150@gmail.com>
2023-07-22 11:22:39 -0500

    feat: :sparkles: se agrega crud de suministros

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ import { CONFIG } from "./config/config.js";
 import appSuministro from "./routers/suministro.js";
 import appGenerar from "./routers/generar.js";
 import appValiddarToken from "./routers/validartoken.js";
+import appFusilAsalto from "./routers/fusil_asalto.js";
 const appExpres = express();
 appExpres.use(express.json());
 
@@ -10,8 +11,8 @@ appExpres.use("/generateToken",appGenerar)
 /* appExpres.use("/validarToken",appValiddarToken) */
 
 
-
-appExpres.use("/suministro",appValiddarToken,appSuministro); //pendiente del enrutador y del jwt
+appExpres.use("/suministro",appSuministro); //pendiente del  dto , proxy y del jwt
+appExpres.use("/fusil-asalto",appFusilAsalto)
 
 appExpres.listen(CONFIG, () => {
   console.log(`http://${CONFIG.hostname}:${CONFIG.port}`);
--- routers/suministro.js
+++ routers/suministro.js
@@ -1,7 +1,7 @@
 import { Router } from "express";
 import mysql from "mysql2";
 import { CONNECT } from "../config/config.js";
-import appValiddarToken from "./validartoken.js";
+
 
 const appSuministro = Router();
 let con = undefined;
@@ -11,26 +11,66 @@ appSuministro.use((req, res, next) => {
   next();
 });
 
-
+//trae toda los suministros de la tabla
 appSuministro.get("/", (req, res) => {
-    con.query(/*sql*/ `SELECT * FROM suministro`, (err, data) => {
+  con.query(/*sql*/ `SELECT * FROM suministro`, (err, data) => {
+    if (err) {
+      res.send(err);
+    } else res.send(data);
+  });
+});
+
+//trae informacion de un suministro en especifico con el id
+
+appSuministro.get("/id/:suministro_id", (req, res) => {
+  con.query(
+    /*sql*/ `SELECT * FROM suministro WHERE suministro_id = ?`,
+    req.params.suministro_id,
+    (err, data) => {
       if (err) {
         res.send(err);
       } else res.send(data);
-    });
-  });
+    }
+  );
+});
+
+//crea un suministro
 
 appSuministro.post("/create", (req, res) => {
   con.query(/*sql*/ `INSERT INTO suministro SET ? `, req.body, (err, data) => {
     if (err) {
       console.log(req.body);
       res.send(err);
-    } else 
-    
-    
-    res.send("creado con exito");
+    } else res.send("creado con exito");
   });
 });
 
+//borra un suministro en especifico con el id
 
-export default appSuministro
+appSuministro.delete("/delete", (req, res) => {
+  con.query(
+    /*sql*/ `DELETE FROM suministro WHERE suministro_id = ?`,
+    req.body.suministro_id,
+    (err, data) => {
+      if (err) {
+        res.send(err);
+      } else res.send(data);
+    }
+  );
+});
+
+//actualioza un suministro en especifico
+
+appSuministro.put("/update", (req, res) => {
+  con.query(
+    /*sql*/ `UPDATE suministro SET ? WHERE suministro_id = ?`,
+    [req.body, req.body.suministro_id],
+    (err, data) => {
+      if (err) {
+        res.send(err);
+      } else res.send(data);
+    }
+  );
+});
+
+export default appSuministro;
