2d3c1386fc504f32adfcc0c643a6173cdb59cadc
Nirclaw <caicedonicolas150@gmail.com>
2023-07-23 16:21:23 -0500

    feat: :sparkles: Se agrega autenticacion a los endpoints

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 import express from "express";
 import { CONFIG } from "./config/config.js";
+const appExpres = express();
+appExpres.use(express.json());
 import appSuministro from "./routers/suministro.js";
-import appGenerar from "./routers/generar.js";
 import appValiddarToken from "./routers/validartoken.js";
 import appFusilAsalto from "./routers/fusil_asalto.js";
 import appEscopetas from "./routers/escopetas.js";
@@ -12,32 +13,32 @@ import appRevolveres from "./routers/revolveres.js";
 import appCarabinas from "./routers/carabinas.js";
 import appGranadas from "./routers/granadas.js";
 import appClaseArmas from "./routers/clase_armas.js";
-
-const appExpres = express();
-appExpres.use(express.json());
+import appGenerar from "./routers/generar.js";
 
 appExpres.use("/generateToken", appGenerar); //generador de token
+
+
 /* appExpres.use("/validarToken",appValiddarToken) */
 
-appExpres.use("/suministro", appSuministro); //pendiente  y del jwt
+appExpres.use("/suministro",appValiddarToken, appSuministro); 
 
-appExpres.use("/fusil-asalto", appFusilAsalto); //pendiente  y del jwt
+appExpres.use("/fusil-asalto",appValiddarToken, appFusilAsalto); 
 
-appExpres.use("/escopetas", appEscopetas); //pendiente  y del jwt
+appExpres.use("/escopetas",appValiddarToken, appEscopetas); 
 
-appExpres.use("/cuchillo-combate", appCuchilloCombate); //pendiente  y del jwt
+appExpres.use("/cuchillo-combate",appValiddarToken, appCuchilloCombate); 
 
-appExpres.use("/pistolas-semi-automaitcas", appPistolasSemiAuto); //pendiente  y del jwt
+appExpres.use("/pistolas-semi-automaitcas",appValiddarToken, appPistolasSemiAuto); 
 
-appExpres.use("/fusil-presicion", appFusilPresicion); //pendiente  y del jwt
+appExpres.use("/fusil-presicion",appValiddarToken, appFusilPresicion); 
 
-appExpres.use("/revolveres", appRevolveres); //pendiente  y del jwt
+appExpres.use("/revolveres",appValiddarToken, appRevolveres); 
 
-appExpres.use("/carabinas", appCarabinas); //pendiente  y del jwt
+appExpres.use("/carabinas",appValiddarToken, appCarabinas); 
 
-appExpres.use("/granadas", appGranadas); //pendiente  y del jwt
+appExpres.use("/granadas",appValiddarToken, appGranadas); 
 
-appExpres.use("/clase-armas", appClaseArmas); //pendiente  y del jwt
+appExpres.use("/clase-armas",appValiddarToken, appClaseArmas); 
 
 appExpres.listen(CONFIG, () => {
   console.log(`http://${CONFIG.hostname}:${CONFIG.port}`);
